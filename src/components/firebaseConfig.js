dd182aed4168e17e6ffce05f0446d7e13cff0708
KarenQuezadaMejia <quezadakaren16@gmail.con>
2019-06-23 17:18:57 -0400

    inicio de sesion

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCk_OUNM_5zVKPxR_kCGNbDzwYyU0XwcRI",
+    authDomain: "reactcards-b7f6e.firebaseapp.com",
+    databaseURL: "https://reactcards-b7f6e.firebaseio.com",
+    projectId: "reactcards-b7f6e",
+    storageBucket: "",
+    messagingSenderId: "605150939738",
+    appId: "1:605150939738:web:2f11897b7840199e"
+};
